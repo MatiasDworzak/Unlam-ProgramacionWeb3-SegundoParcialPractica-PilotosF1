ac316f34fadf7641c8b907421c92f4439e5bd996
Mati <matu.dw@gmail.com>
2026-03-05 01:47:27 -0300

    prueba para deploy

--- GestionF1.Web/Program.cs
+++ GestionF1.Web/Program.cs
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<GestionF1Context>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<GestionF1Context>(options =>options.UseNpgsql
+(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IPilotoServicio, PilotoServicio>();
 builder.Services.AddScoped<IEscuderiasServicio, EscuderiasServicio>();
